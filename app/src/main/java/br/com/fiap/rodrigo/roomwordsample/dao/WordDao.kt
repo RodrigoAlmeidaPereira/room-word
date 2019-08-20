3b5be57c5df0462b20be7d63f7037be38948c7aa
Logon Aluno <logonaluno@local.com>
2019-08-19 21:00:30 -0300

    finished

---
+++ app/src/main/java/br/com/fiap/rodrigo/roomwordsample/dao/WordDao.kt
@@ -0,0 +1,22 @@
+package br.com.fiap.rodrigo.roomwordsample.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import br.com.fiap.rodrigo.roomwordsample.model.Word
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAllWords(): LiveData<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    fun deleteAll()
+
+}
