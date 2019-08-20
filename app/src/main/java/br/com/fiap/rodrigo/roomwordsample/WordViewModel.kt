28306d99abdf4f07e6b53aef7a75dbf3bd270b8e
Logon Aluno <logonaluno@local.com>
2019-08-19 22:34:53 -0300

    Removed unused imports

--- app/src/main/java/br/com/fiap/rodrigo/roomwordsample/WordViewModel.kt
+++ app/src/main/java/br/com/fiap/rodrigo/roomwordsample/WordViewModel.kt
@@ -4,15 +4,13 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
-import br.com.fiap.rodrigo.roomwordsample.dao.WordDao
-import br.com.fiap.rodrigo.roomwordsample.dao.WordRoomDatabase
 import br.com.fiap.rodrigo.roomwordsample.model.Word
 import br.com.fiap.rodrigo.roomwordsample.repository.WordRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class WordViewModel(application: Application, private val repository: WordRepository)
-    : AndroidViewModel(application) {
+class WordViewModel(application: Application,
+                    private val repository: WordRepository) : AndroidViewModel(application) {
 
     val allWords: LiveData<List<Word>> = repository.allWords
 
