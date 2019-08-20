e55c42e156c3c699e1e39c61337c932412e87b0d
Logon Aluno <logonaluno@local.com>
2019-08-19 22:28:32 -0300

    Removed unused import

--- app/src/main/java/br/com/fiap/rodrigo/roomwordsample/di/Modules.kt
+++ app/src/main/java/br/com/fiap/rodrigo/roomwordsample/di/Modules.kt
@@ -3,7 +3,6 @@ package br.com.fiap.rodrigo.roomwordsample.di
 import androidx.room.Room
 import br.com.fiap.rodrigo.roomwordsample.WordListAdapter
 import br.com.fiap.rodrigo.roomwordsample.WordViewModel
-import br.com.fiap.rodrigo.roomwordsample.dao.WordDao
 import br.com.fiap.rodrigo.roomwordsample.dao.WordRoomDatabase
 import br.com.fiap.rodrigo.roomwordsample.repository.WordRepository
 import org.koin.android.viewmodel.dsl.viewModel
